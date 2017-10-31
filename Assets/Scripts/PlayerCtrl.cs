143d6408089d45b5dab3e001ad980fdec92f094a
Antonio Junqueira <antoniojunqueira.cg@gmail.com>
2017-10-31 10:46:01 -0200

    pulo duplo

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -35,6 +35,10 @@ public class PlayerCtrl : MonoBehaviour {
 
 	public LayerMask whatIsGround;
 
+	bool canDoubleJump = false;
+	
+	public float delayForDoubleJump = 0.2f;
+
 
 	// Use this for initialization
 	void Start () {
@@ -101,7 +105,20 @@ public class PlayerCtrl : MonoBehaviour {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+
+		Invoke("EnableDoubleJump", delayForDoubleJump);
 	}
+	
+	if (canDoubleJump && !isGrounded) {
+		rb.velocity = Vector2.zero;
+		rb.AddForce(new Vector2(0f, jumpSpeed));
+		anim.SetInteger("State", 1);
+		canDoubleJump = false;
+	}
+	}
+
+	void EnableDoubleJump() {
+		canDoubleJump = true;
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
