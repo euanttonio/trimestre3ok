a75dd568d45dec796fc36c824faecf227b684512
Antonio Junqueira <antoniojunqueira.cg@gmail.com>
2017-10-31 10:40:41 -0200

    pulo simples

--- Assets/Scripts/PlayerCtrl.cs
+++ Assets/Scripts/PlayerCtrl.cs
@@ -25,6 +25,16 @@ public class PlayerCtrl : MonoBehaviour {
 
 	bool isJumping = false;
 
+	public Transform feet;
+
+	public float feetWidth = 0.5f;
+
+	public float feetHeight = 0.1f;
+
+	public bool isGrounded;
+
+	public LayerMask whatIsGround;
+
 
 	// Use this for initialization
 	void Start () {
@@ -32,10 +42,16 @@ public class PlayerCtrl : MonoBehaviour {
 		sr = GetComponent<SpriteRenderer>();
 		anim = GetComponent<Animator>();
 	}
+
+	void OnDrawGizmos() {
+		Gizmos.DrawWireCube(feet.position, new Vector3(0.5f, 0.1f, 0f));
+	}
 	
 	// Update is called once per frame
 	void Update () {
 		
+		isGrounded = Physics2D.OverlapBox(new Vector2(feet.position.x, feet.position.y), new Vector2(feetWidth, feetHeight), 360.0f, whatIsGround);
+
 		float horizontalInput = Input.GetAxisRaw("Horizontal"); // -1: esquerda, 1: direita
 		float horizontalPlayerSpeed = horizontalSpeed * horizontalInput;
 		if (horizontalPlayerSpeed != 0) {
@@ -81,9 +97,11 @@ public class PlayerCtrl : MonoBehaviour {
 	}
 
 	void Jump() {
+		if (isGrounded) {
 		isJumping = true;
 		rb.AddForce(new Vector2(0f, jumpSpeed));
 		anim.SetInteger("State", 1);
+	}
 	}
 
 	void OnCollisionEnter2D(Collision2D other) {
